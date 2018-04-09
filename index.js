d5478972f8a09850c20a33e90a0bd8f565b36692
TAIWO KOLAWOLE <kolawole.taiwo@andela.com>
2018-04-09 15:05:29 +0100

    feature(html-interface): add ui to app - add support for ejs template engine

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.status(200).send('Hello');
+  res.status(200).render('index');
 });
 
 app.listen(3000, () => console.log('App running on port 3000'));
