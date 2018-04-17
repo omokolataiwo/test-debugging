e9b878fba5117b6fbfc30665f68bd032ae37ad4a
TAIWO KOLAWOLE <kolawole.taiwo@andela.com>
2018-04-17 13:01:07 +0100

    Implemented feature

--- utils/OX.js
+++ utils/OX.js
@@ -1,2 +1,14 @@
-const OX = inputStr => {};
+const OX = inputStr => {
+  // Don't do anything for non string
+  if (typeof inputStr !== 'string') return true;
+
+  let output = 0;
+  inputStr.split('').forEach(letter => {
+    // Convert all letters to lower case
+    letter = letter.toLowerCase();
+    if (letter === 'x') output += 1;
+    else if (letter === 'o') output -= 1;
+  });
+  return output === 0;
+};
 export default OX;
