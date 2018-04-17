989b4292d3ce77f6f14c551dd7d80d1bc003bf72
TAIWO KOLAWOLE <kolawole.taiwo@andela.com>
2018-04-17 12:58:16 +0100

    Failing test

--- utils/OX.js
+++ utils/OX.js
@@ -1,14 +1,2 @@
-const OX = inputStr => {
-  // Don't do anything for non string
-  if (typeof inputStr !== 'string') return true;
-
-  let output = 0;
-  inputStr.split('').forEach((letter) => {
-    // Convert all letters to lower case
-    letter = letter.toLowerCase();
-    if (letter === 'x') output += 1;
-    else if (letter === 'o') output -= 1;
-  })
-  return output === 0;
-};
+const OX = inputStr => {};
 export default OX;
